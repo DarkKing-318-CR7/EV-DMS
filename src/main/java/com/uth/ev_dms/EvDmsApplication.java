02346f6f56e493b3092f77084d6a925ac5f32c43
Chi <huuchinguyen241@gmail.com>
2025-10-08 09:44:51 +0700

    first commit

---
+++ src/main/java/com/uth/ev_dms/EvDmsApplication.java
@@ -0,0 +1,13 @@
+package com.uth.ev_dms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvDmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvDmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uth/ev_dms/EvDmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uth.ev_dms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvDmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
